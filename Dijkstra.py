05843e1d8fd57ff035c3997d1053547867d99950
pranavhegde2000 <55937095+pranavhegde2000@users.noreply.github.com>
2021-04-08 18:49:03 +0530

    Update Dijkstra.py

--- Dijkstra.py
+++ Dijkstra.py
@@ -1,20 +1,31 @@
+# For anyone not familiar with Dijstra's algorithm,
+# it can be considered as the OG shortest path/path-finding
+# algorithm, its a single source shortest path algorithm(SSSP)
+# So it can be used to find the shortest path in graph, for this program
+# an adjacency matrix will be used to represent the Graph
 import sys
 
 
 class Graph:
-    def __init__(self, vertices):
+    # Constructor
+    def __init__(
+        self, vertices
+    ):  # Self is a keyword in Python that is an instance of a class, with a dot operator it can be
+        # used to access/manipulate elements of the class
         self.V = vertices
         self.grh = [[0 for column in range(vertices)] for row in range(vertices)]
 
+    # Function to print the elements of the adjacency matrix
     def printsolution(self, dist):
         print("Vertex \tDistance from Source")
         for node in range(self.V):
             print(node, "\t", dist[node])
 
-    # A utility function to find the vertex with
+    # The following function is used to find the vertex with
     # minimum distance value, from the set of vertices
     # not yet included in shortest path tree
-    def mindistance(self, dist, sptSet):
+    # The shortest path tree is represented by the shortest_path_set array
+    def mindistance(self, dist, shortest_path_Set):
 
         # Initilaize minimum distance for next node
         minimum = sys.maxsize
@@ -22,31 +33,31 @@ class Graph:
         # Search not nearest vertex not in the
         # shortest path tree
         for v in range(self.V):
-            if dist[v] < minimum and sptSet[v] == False:
+            if dist[v] < minimum and shortest_path_Set[v] == False:
                 minimum = dist[v]
                 min_index = v
 
         return min_index
 
-    # Funtion that implements Dijkstra's single source
-    # shortest path algorithm for a graph represented
-    # using adjacency matrix representation
+    # The following function implements Dijkstra's SSSP
+    # algorithm for a graph which is represented
+    # using an adjacency matrix, an adjacency matrix is the most easiest 
     def dijkstra(self, src):
 
         dist = [sys.maxsize] * self.V
         dist[src] = 0
-        sptSet = [False] * self.V
+        shortest_path_Set = [False] * self.V
 
         for cout in range(self.V):
 
             # Pick the minimum distance vertex from
             # the set of vertices not yet processed.
             # u is always equal to src in first iteration
-            u = self.mindistance(dist, sptSet)
+            u = self.mindistance(dist, shortest_path_Set)
 
             # Put the minimum distance vertex in the
             # shotest path tree
-            sptSet[u] = True
+            shortest_path_Set[u] = True
 
             # Update dist value of the adjacent vertices
             # of the picked vertex only if the current
@@ -55,7 +66,7 @@ class Graph:
             for v in range(self.V):
                 if (
                     self.grh[u][v] > 0
-                    and sptSet[v] == False
+                    and shortest_path_Set[v] == False
                     and dist[v] > dist[u] + self.grh[u][v]
                 ):
                     dist[v] = dist[u] + self.grh[u][v]
